36cffe8856f2cbe9fa995988750717deff7e76a6
stuarthillary <shillary.git@gmail.com>
2023-12-09 22:21:08 +0000

    Initial commit
    Code as stands at end of YouTube video https://www.youtube.com/watch?v=okXbHqRQ6Mo

---
+++ Queues/Program.cs
@@ -0,0 +1,167 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+
+var cts = new CancellationTokenSource();
+var startColour = Console.ForegroundColor;
+Console.CancelKeyPress += (_, _) => { 
+    cts.Cancel();
+    Console.ForegroundColor = startColour;
+};
+
+var simulation = new DrivingToDisneyLand(cts.Token);
+await simulation.Simulate();
+
+public class DrivingToDisneyLand
+{
+    private readonly CancellationToken _cancellationToken;
+    private readonly Road _road;
+    private readonly Driver _driver;
+    private readonly Passenger _passenger;
+
+    public DrivingToDisneyLand(CancellationToken cancellationToken)
+    {
+        _cancellationToken = cancellationToken;
+        _driver = new Driver(this);
+        _road = new Road(this);
+        _passenger = new Passenger(this);
+    }
+
+    class Road
+    {
+        private readonly DrivingToDisneyLand _context;
+
+        public Road(DrivingToDisneyLand context)
+        {
+            _context = context;
+        }
+        
+        public async Task Drive()
+        {
+            while (!_context._cancellationToken.IsCancellationRequested)
+            {
+                var danger = new[]
+                {
+                    "passing truck",
+                    "cat on the road",
+                    "sub shining in the eye",
+                    "drunk driver",
+                    "ambulance",
+                };
+
+                await _context._driver.Send(
+                    new Driver.Alert(
+                        danger[Random.Shared.Next(0, danger.Length)],
+                        Random.Shared.Next(3,7)
+                    )
+                );
+
+                await Task.Delay(TimeSpan.FromSeconds(Random.Shared.Next(1, 5)), _context._cancellationToken);
+            }
+        }
+    }
+
+    class Driver
+    {
+        private Channel<IMessage> _channel = Channel.CreateUnbounded<IMessage>();
+
+        public async Task Listen()
+        {
+            while (!_context._cancellationToken.IsCancellationRequested)
+            {
+                var msg = await _channel.Reader.ReadAsync();
+
+                switch (msg)
+                {
+                    case Alert alert:
+                        await ProcessAlert(alert);
+                        break;
+                    case AskQuestion askQuestion:
+                        await ProcessAskQuestion(askQuestion);
+                        break;
+                }
+            }
+        }
+
+        public ValueTask Send<T>(T msg) where T: IMessage => _channel.Writer.WriteAsync(msg);
+
+        public interface IMessage { }
+
+        public abstract record AwaitableMessage<TReturn>
+        {
+            private TaskCompletionSource<TReturn> _resolve = new();
+            public Task<TReturn> Result() => _resolve.Task;
+            public void Complete(TReturn result) => _resolve.SetResult(result);
+        }
+        
+        public record Alert(string Danger, int DurationSeconds) : IMessage;
+        public record AskQuestion(string Question) : AwaitableMessage<string>, IMessage;
+        
+        private readonly DrivingToDisneyLand _context;
+
+        public Driver(DrivingToDisneyLand context)
+        {
+            _context = context;
+        }
+
+        private async Task ProcessAlert(Alert alert)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"paying attention to {alert.Danger}");
+
+            await Task.Delay(TimeSpan.FromSeconds(alert.DurationSeconds), _context._cancellationToken);
+            
+            Console.WriteLine($"finished paying attention to {alert.Danger}");
+        }
+
+        private async Task ProcessAskQuestion(AskQuestion question)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"processing \"{question.Question}\"");
+
+            await Task.Delay(TimeSpan.FromSeconds(Random.Shared.Next(1, 3)), _context._cancellationToken);
+
+            Console.WriteLine($"answering {question.Question}");
+            
+            question.Complete("Pffft");
+        }
+    }
+
+    class Passenger
+    {
+        private readonly DrivingToDisneyLand _context;
+
+        public Passenger(DrivingToDisneyLand context)
+        {
+            _context = context;
+        }
+
+        public async Task BeBored()
+        {
+            while (!_context._cancellationToken.IsCancellationRequested)
+            {
+                var questions = new[]
+                {
+                    "are we there yet?",
+                    "can we stop at McDonalds?",
+                    "I'm hungry",
+                    "I'm bored",
+                };
+
+                var msg = new Driver.AskQuestion(
+                    questions[Random.Shared.Next(0, questions.Length)]
+                );
+                
+                await _context._driver.Send(msg);
+                var answer = await msg.Result();
+
+                await Task.Delay(TimeSpan.FromSeconds(Random.Shared.Next(1, 5)), _context._cancellationToken);
+            }
+        }
+    }
+
+    public Task Simulate()
+    {
+        return Task.WhenAll(_road.Drive(), _driver.Listen(), _passenger.BeBored());
+    }
+}
---
+++ Semaphores/Program.cs
@@ -0,0 +1,126 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_,_) => {cts.Cancel();};
+
+var simulation = new DrivingToDisneyLand(cts.Token);
+await simulation.Simulate();
+
+public class DrivingToDisneyLand
+{
+    private readonly CancellationToken _cancellationToken;
+    private readonly Road _road;
+    private readonly Driver _driver;
+    private readonly Passenger _passenger;
+
+    public DrivingToDisneyLand(CancellationToken cancellationToken)
+    {
+        _cancellationToken = cancellationToken;
+        _driver = new Driver(this);
+        _road = new Road(this);
+        _passenger = new Passenger(this);
+    }
+
+    class Road
+    {
+        private readonly DrivingToDisneyLand _context;
+
+        public Road(DrivingToDisneyLand context)
+        {
+            _context = context;
+        }
+        
+        public async Task Drive()
+        {
+            while (!_context._cancellationToken.IsCancellationRequested)
+            {
+                var danger = new[]
+                {
+                    "passing truck",
+                    "cat on the road",
+                    "sub shining in the eye",
+                    "drunk driver",
+                    "ambulance",
+                };
+
+                await _context._driver.Alert(
+                    danger[Random.Shared.Next(0, danger.Length)],
+                    Random.Shared.Next(3,7)
+                );
+
+                await Task.Delay(TimeSpan.FromSeconds(Random.Shared.Next(1, 5)), _context._cancellationToken);
+            }
+        }
+    }
+
+    class Driver
+    {
+        private readonly DrivingToDisneyLand _context;
+        private SemaphoreSlim _busy = new SemaphoreSlim(1);
+        
+        public Driver(DrivingToDisneyLand context)
+        {
+            _context = context;
+        }
+
+        public async Task Alert(string danger, int durationSeconds)
+        {
+            await _busy.WaitAsync();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"paying attention to {danger}");
+
+            await Task.Delay(TimeSpan.FromSeconds(durationSeconds), _context._cancellationToken);
+            
+            Console.WriteLine($"finished paying attention to {danger}");
+            _busy.Release();
+        }
+
+        public async Task<string> AskQuestion(string question)
+        {
+            await _busy.WaitAsync();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"processing \"{question}\"");
+
+            await Task.Delay(TimeSpan.FromSeconds(Random.Shared.Next(1, 3)), _context._cancellationToken);
+
+            Console.WriteLine($"answering {question}");
+            _busy.Release();
+            
+            return "Pffft";
+        }
+    }
+
+    class Passenger
+    {
+        private readonly DrivingToDisneyLand _context;
+
+        public Passenger(DrivingToDisneyLand context)
+        {
+            _context = context;
+        }
+
+        public async Task BeBored()
+        {
+            while (!_context._cancellationToken.IsCancellationRequested)
+            {
+                var questions = new[]
+                {
+                    "are we there yet?",
+                    "can we stop at McDonalds?",
+                    "I'm hungry",
+                    "I'm bored",
+                };
+
+                var answer = await _context._driver.AskQuestion(
+                    questions[Random.Shared.Next(0, questions.Length)]);
+
+                await Task.Delay(TimeSpan.FromSeconds(Random.Shared.Next(1, 5)), _context._cancellationToken);
+            }
+        }
+    }
+
+    public Task Simulate()
+    {
+        return Task.WhenAll(_road.Drive(), _passenger.BeBored());
+    }
+}
